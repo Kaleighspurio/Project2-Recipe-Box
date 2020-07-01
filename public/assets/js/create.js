611878978c936a5f135830f5462ba5382fb3254f
Karen Astell <KarenAstell@gmail.com>
2020-07-01 15:23:28 -0400

    commented out function in create.js so travis will be happy

--- public/assets/js/create.js
+++ public/assets/js/create.js
@@ -1,5 +1,5 @@
 // why does travis not like this? I use loadFile on my html
-const loadFile = (event) => {
-  const image = document.getElementById('output');
-  image.src = URL.createObjectURL(event.target.files[0]);
-};
+// const loadFile = (event) => {
+//   const image = document.getElementById('output');
+//   image.src = URL.createObjectURL(event.target.files[0]);
+// };
