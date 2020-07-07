569bbffb0af5d59bbd3485639869954896c79738
Bryan Pero <bryanpero@gmail.com>
2020-07-07 11:22:43 -0400

    bryan index html/index js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,2 +1,52 @@
-$(document).ready(() => {
+/*$(document).ready(function () {
+  $("search-button").click(function () {
+    const test = $("#search-ingredient").val();
+    console.log(test);
+  });
+}); */
+
+$(document).ready(function(){
+  $(".ingredient-button").click(function(){
+    $.ajax({url: "api/main", success: function(result){
+      $("#match-list").html(result)
+    }});
+  });
+});
+
+$.ajax({
+  method: 'GET',
+  url: '/api/main',
+}).then((response) => {
+  console.log(response);
+  
+//click handler
+$(".ingredient-button").on("click",(event) => {
+  event.preventDefault();
+  clear();
+
+  $.ajax({
+    url: 'api/main',
+    method: "GET"
+  }).then(updatePage);
+});
+
+//  .on("click") function associated with the clear button
+$("#clear-all").on("click", clear);    
+
+/* $.get('api/main').then () => (response) {
+      console.log(response);
+    };
+
+    displayMatches = () => {
+
+    }
+
+    searchRecipes = () => {
+      
+      var queryURL = "api/main";
+      
+      queryParams.q = $("#search-term")
+        .val()
+        .trim();
     
+      
