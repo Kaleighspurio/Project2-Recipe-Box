7b02a8f23cc736b0c758a0fa16cd7547aad3e1a6
Bryan Pero <bryanpero@gmail.com>
2020-07-07 21:28:14 -0400

    worked ploddingly on view.js/html

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,13 +1,27 @@
 const url = new URL(window.location);
-const id = url.searchParams.get('id');
+const id = url.searchParams.get("id");
 
 if (id) {
   // search for the recipe
   $.ajax({
-    method: 'GET',
+    method: "GET",
     url: `api/view/${id}`,
   }).then((response) => {
     console.log(response);
-    // update placeholders
+    // update placeholder text in recipe card //
+    $(".recipe-name").text("Recipe Title", `${recipe.recipe_name}`);
+    $(".recipe-author").text("Recipe Author", `${recipe.Author.name}`);
+    $(".recipe-instructions").text(
+      "Recipe Instructions...",
+      `${recipe.instructions}`
+    );
+    $(".recipe-link").text("Recipe link", `${recipe.link}`);
   });
 }
+
+// create click event for add to favorites button
+$(".add-favorite").on("click", (event) => {
+  addFavorite(recipe);}
+// creat click event for email button
+$(".email-recipe").on("click", (event) => {
+  emailRecipe(recipe);}
