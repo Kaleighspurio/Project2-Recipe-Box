4c37e6aae0823bd4599aec343755312c785323ab
Lynn Boudreau <lw.boudreau@live.com>
2020-07-11 11:52:22 -0400

    added seeders

--- seeders/20200701150359-ingredients.js
+++ seeders/20200701150359-ingredients.js
@@ -161,6 +161,406 @@ module.exports = {
     createdAt: new Date(),
     updatedAt: new Date(),
     RecipeId: 3,
+  }, {
+    ingredient_name: '1 clove garlic, finely chopped',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 4,
+  }, {
+    ingredient_name: '2 tablespoons margarine or butter, softened',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 4,
+  }, {
+    ingredient_name: '1 teaspoon chopped fresh or 1/2 teaspoon dried oregano',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 4,
+  }, {
+    ingredient_name: '1/4 teaspoon paprika',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 4,
+  }, {
+    ingredient_name: 'dash of freshly ground pepper',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 4,
+  }, {
+    ingredient_name: '2 tablespoons olive oil',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 5,
+  }, {
+    ingredient_name: '1/4 teaspoon salt',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 5,
+  }, {
+    ingredient_name: '1/8 teaspoon ground red cayenne pepper',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 5,
+  }, {
+    ingredient_name: '1 clove garlic, finely chopped',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 5,
+  }, {
+    ingredient_name: '4 halibut steaks, 1 inch thick (about 1 1/2 pounds)',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 5,
+  }, {
+    ingredient_name: 'chopped fresh cilantro, if desired',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 5,
+  }, {
+    ingredient_name: '5 cloves garlic, coarsely chopped',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 6,
+  }, {
+    ingredient_name: '1/4 cup lemon juice',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 6,
+  }, {
+    ingredient_name: '2 tablespoons chopped fresh parsley',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 6,
+  }, {
+    ingredient_name: '1/4 teaspoon salt',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 6,
+  }, {
+    ingredient_name: '1/2 cup olive oil',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 6,
+  }, {
+    ingredient_name: '1 pound cod (about 1/4 inch thick)',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 7,
+  }, {
+    ingredient_name: '2 cups cut cooked green beans',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 7,
+  }, {
+    ingredient_name: '2 tablespoons chopped fresh parsley',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 7,
+  }, {
+    ingredient_name: '1 pound extra lean ground beef',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 8,
+  }, {
+    ingredient_name: '1 cup finely diced onion',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 8,
+  }, {
+    ingredient_name: '1 teaspoon minced garlic',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 8,
+  }, {
+    ingredient_name: '1 teaspoon minced ginger',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 8,
+  }, {
+    ingredient_name: '3/4 cup thick Teriaki sauce',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 8,
+  }, {
+    ingredient_name: '1 cup water',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 9,
+  }, {
+    ingredient_name: '1/4 cup soy sauce',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 9,
+  }, {
+    ingredient_name: '5 teaspoons packed brown sugar',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 9,
+  }, {
+    ingredient_name: '1 tablespoon honey',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 9,
+  }, {
+    ingredient_name: '1/2 teaspoon ground ginger',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 9,
+  }, {
+    ingredient_name: '1/4 teaspoon garlic powder',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 9,
+  }, {
+    ingredient_name: '2 tablespoons corn starch',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 9,
+  }, {
+    ingredient_name: '1/4 cup cold water',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 9,
+  }, {
+    ingredient_name: '4 English muffins',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 10,
+  }, {
+    ingredient_name: '1/4 cup (1/2 stick) margarine or butter, softened',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 10,
+  }, {
+    ingredient_name: '1 tablespoon honey',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 10,
+  }, {
+    ingredient_name: '1/2 teaspoon grated lemon peel',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 10,
+  }, {
+    ingredient_name: '1/4 cup red wine vinegar',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: '2 tablespoons vegetable oil',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: '1/2 teaspoon chili powder',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: '1/4 teaspoon ground cumin',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: '1 small clove garlic, crushed',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: 'chili dressing',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: '1 cup cooked whole kernel corn',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: '1 cup diced jicama',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: '3/4 cup tomato, seeded and chopped',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: '30 ounces black beans, cooked',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: '2 green onions with tops sliced',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 11,
+  }, {
+    ingredient_name: '4 ounces diced green chilies, drained',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 12,
+  }, {
+    ingredient_name: '8 flour tortillas, 7-8 inches in diameter',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 12,
+  }, {
+    ingredient_name: '1 cup shredded Cheddar cheese',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 12,
+  }, {
+    ingredient_name: '1 dozen large eggs ',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 13,
+  }, {
+    ingredient_name: '2 teaspoons Dijon mustard',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 13,
+  }, {
+    ingredient_name: '1/3 cup mayonnaise',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 13,
+  }, {
+    ingredient_name: '1 tablespoon minced shallot or onion',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 13,
+  }, {
+    ingredient_name: '1/4 teaspoon Tabasco sauce',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 13,
+  }, {
+    ingredient_name: 'salt and pepper to taste',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 13,
+  }, {
+    ingredient_name: 'paprika to taste',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 13,
+  }, {
+    ingredient_name: 'non-stick vegetable oil spray',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '1 cup unsweetened shredded coconut',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '1 1/2 cups white rice flour',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '3/4 cups sorghum',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '3/4 cup tapioca starch',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '1 1/2 teaspoons baking powder',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '1 1/2 teaspoons baking soda',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '3/4 teaspon kosher salt',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '1/2 teaspoon xantham gum',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '1 1/2 cups sugar',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '1 1/2 cups unsweetened coconut milk',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '3/4 cup vegetable oil',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '1 teaspoon coconut extract',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '4 large eggs',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '1/4 cup unsweetened coconut flakes',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 14,
+  }, {
+    ingredient_name: '6 eggs',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 15,
+  }, {
+    ingredient_name: '1/4 cup milk any kind',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 15,
+  }, {
+    ingredient_name: 'salt and pepper to taste',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 15,
+  }, {
+    ingredient_name: '1/2 green pepper diced',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 15,
+  }, {
+    ingredient_name: '4 tablespoons unsalted butter divided',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 15,
+  }, {
+    ingredient_name: '6 slices bacon cooked, drained, and roughly chopped',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 15,
+  }, {
+    ingredient_name: '3/4 cup shredded cheddar cheese divided',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 15,
+  }, {
+    ingredient_name: '6 medium soft tortilla shells',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    RecipeId: 15,
   }], {}),
 
 
